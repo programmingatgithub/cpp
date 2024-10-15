318d12a988a4b064a0f4d9420f9f55f0af3d0f97
Mayank Verma <80618915+programmingatgithub@users.noreply.github.com>
2024-10-15 17:32:07 +0530

    Add files via upload

---
+++ inputandoutput.cpp
@@ -0,0 +1,19 @@
+// problem 3: cpp (source : hackerrank).
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+int a;
+int b;
+int c;
+cin >> a >> b >> c;
+int sum;
+sum = a + b + c;
+cout << sum << endl;
+    return 0;
+}
